59b10c2ebe06b31dbfc0fd40fefa5baba34e08f5
QuanLe11 <quanle61500@gmail.com>
2021-03-26 16:35:10 -0400

    Lab 6 Progress: Tried Stuff Out to Populate Table

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,33 @@
 async function diningData() {
   // fetch request
-  const request = await fetch('/api/dining');
+  const request = await fetch('/api/macros');
+  const data = request.json();
+  console.table(data);
+
+  const diningHall = data;
+  console.log(typeof diningHall);
+  const target = document.querySelector('#target');
+  for(Object.entries(diningHall)) {
+    const mealID = item.meal_id;
+    /* const name = item.hall_name; */
+    const calories = item.calories;
+    const carbs = item.carbs;
+    const sodium = item.sodium;
+    const protein = item.protein;
+    const fat = item.fat;
+    const cholesterol = item.cholesterol;
+    target.innerHTML += `
+            <tr>
+                <th>${mealID}</th>
+                <td>${calories}</td>
+                <td>${carbs}</td>
+                <td>${sodium}</td>
+                <td>${protein}</td>
+                <td>${fat}</td>
+                <td>${cholesterol}</td>
+            </tr>
+            `;
+  };
 }
+
+diningData();
